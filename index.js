b5f9cf1e8daa344ab923ed3a9bcb9e045ca7790d
F1Zuwu <113283931+F1Zuwu@users.noreply.github.com>
2024-09-23 09:57:42 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -8,10 +8,30 @@ const city = "Tartu"
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityname}&appid=${key}`)
         .then((response) => {
             return response.json()
         })
